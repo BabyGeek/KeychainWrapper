8dc596a17d0347dda62e0a986e8831e3552b0943
OGGERO Paul <contact@poggero.com>
2023-01-11 22:31:01 +0100

    feat(test): added unit tests

--- Tests/KeychainWrapperTests/KeychainWrapperTests.swift
+++ Tests/KeychainWrapperTests/KeychainWrapperTests.swift
@@ -2,10 +2,78 @@ import XCTest
 @testable import KeychainWrapper
 
 final class KeychainWrapperTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(KeychainWrapper().text, "Hello, World!")
+    let testKey = "primitiveValueTestKey"
+    let testString: String = "testValue"
+    
+    func testRetrieveEmptyKeychainShouldThrowError() throws {
+        do {
+            let _ = try KeychainWrapper.get(account: testKey)
+        } catch {
+            XCTAssertEqual(error as? KeychainWrapperError, .operationError)
+        }
+    }
+    
+    func testDeleteEmptyKeychainShouldThrowError() throws {
+        do {
+            let _ = try KeychainWrapper.delete(account: testKey)
+        } catch {
+            XCTAssertEqual(error as? KeychainWrapperError, .operationError)
+        }
+    }
+    
+    func testSaveString() throws {
+        guard let data = testString.data(using: .utf8) else {
+            XCTFail("Could not convert string to data using UTF8")
+            return
+        }
+        
+        do {
+            try KeychainWrapper.set(value: data, account: testKey)
+            XCTAssertTrue(true)
+        } catch {
+            XCTFail("Failed to set value")
+        }
+    }
+    
+    func testRetrieveString() throws {
+        try testSaveString()
+        try testSaveString()
+        
+        guard let data = try KeychainWrapper.get(account: testKey) else {
+            XCTFail("Could not retrieve keychain")
+            return
+        }
+        
+        XCTAssertEqual(String(data: data, encoding: .utf8), testString)
+    }
+    
+    func testRetrieveErrorString() throws {
+        do {
+            let _ = try KeychainWrapper.get(account: testKey)
+        } catch {
+            XCTAssertEqual(error as? KeychainWrapperError, .operationError)
+        }
+    }
+    
+    func testDeleteString() throws {
+        try testSaveString()
+        
+        do {
+            try KeychainWrapper.delete(account: testKey)
+            XCTAssertTrue(true)
+        } catch {
+            XCTFail("Failed to delete value")
+        }
+    }
+    
+    func testDeleteAll() throws {
+        try testSaveString()
+        
+        do {
+            try KeychainWrapper.deleteAll()
+            XCTAssertTrue(true)
+        } catch {
+            XCTFail("Failed to delete all")
+        }
     }
 }
